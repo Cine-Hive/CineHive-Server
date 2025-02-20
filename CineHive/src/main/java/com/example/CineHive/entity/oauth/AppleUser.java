9c562d7470e31f279131583e624baeac5dd3e091
jbg <kjbg4565388@gmail.com>
2025-02-20 14:15:33 +0900

    [Add]: AppleUser 엔티티 생성

---
+++ CineHive/src/main/java/com/example/CineHive/entity/oauth/AppleUser.java
@@ -0,0 +1,50 @@
+package com.example.CineHive.entity.oauth;
+
+import com.example.CineHive.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppleUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50)
+    private String nickname;
+
+    @Column(name = "mem_email", nullable = true)
+    private String memEmail;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    // User 엔티티와의 관계 설정
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "mem_id", insertable = false, updatable = false)
+    private User user;
+
+    @Column
+    private String name;
+
+    @ElementCollection
+    @CollectionTable(name = "apple_user_genres", joinColumns = @JoinColumn(name = "apple_user_id"))
+    @Column(name = "genre")
+    private List<String> genres;
+
+
+    public AppleUser(String nickname, String memEmail, String name, List<String> genres) {
+        this.nickname = nickname;
+        this.memEmail = memEmail;
+        this.name = name;
+        this.genres = genres;
+    }
+}
