a16013d2cc59a37f7d98c76bbfe3ceb3b39c9987
jbg <kjbg4565388@gmail.com>
2025-03-14 02:43:57 +0900

    [Chore]: 주석 추가

--- CineHive/src/main/java/com/example/CineHive/controller/oauth/app/RequestKakaoAppController.java
+++ CineHive/src/main/java/com/example/CineHive/controller/oauth/app/RequestKakaoAppController.java
@@ -22,6 +22,8 @@ import java.util.Map;
 /*
 앱에서 SDK를 실행해서 인증 및 로그인이 성공된 후, Access Token을 담아서 요청을 보내면 서버에서 json 데이터를 클라이언트에게 보냄
 이때, 신규 회원이면 201로 반환하여 추가 기입 정보를 반환, 200이면 바로 로그인 (앱에서 201과 200으로 처리하면 될 거 같음)
+
+*** KakaoUser 테이블 -> User 테이블 하나로 관리하기 위해 관련 코드 수정 필요 ***
  */
 @Tag(name = "Kakao User App Controller", description = "카카오 앱 로그인 API 관련 기능을 제공하는 API")
 @Slf4j
