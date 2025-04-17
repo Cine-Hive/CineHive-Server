29cacadf502a99002090ca383e1b640862b8a21c
Jong MIn <unib335@naver.com>
2025-04-18 01:59:14 +0900

    [Ref]: 기존 중복 코드 및 API 구조 정리를 위해 레거시 제거
    - 영화, 애니메이션, 드라마(TV) 관련 분산 되어 있던 Repository, Service, Controller, Entity, dto 제거

--- CineHive/src/main/java/com/example/CineHive/repository/videos/animation/AnimationDirectorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.CineHive.repository.videos.animation;
-
-import com.example.CineHive.entity.credit.animation.Director;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnimationDirectorRepository extends JpaRepository<Director,Long> {
-    Director findByName(String name);
-}
