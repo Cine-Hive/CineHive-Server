46d812f03aec1f46ddf1590f81cdc08753fb8bac
Jong MIn <unib335@naver.com>
2025-04-18 21:49:06 +0900

    [Ref]: VideoDto 클래스 수정 및 Swagger 메타데이터 추가
    - VideoDto 클래스의 필드 이름 변경 및 추가: id, name, key, site, type
    - Swagger 어노테이션을 활용하여 각 필드에 대한 설명 및 예시 추가
    - Lombok 어노테이션을 @Data, @Builder로 변경하여 코드 간결화

--- CineHive/src/main/java/com/example/CineHive/dto/media/VideoDto.java
+++ CineHive/src/main/java/com/example/CineHive/dto/media/VideoDto.java
@@ -1,20 +1,29 @@
 package com.example.CineHive.dto.media;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import java.util.List;
-
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@Schema(description = "비디오 정보")
 public class VideoDto {
-    private Long id;
-    private String posterPath;
-    private String title;
-    private String releaseDate;
-    private List<String> genres;
+    @Schema(description = "비디오 ID", example = "5c9294240e0a267cd516835f")
+    private String id;
+    
+    @Schema(description = "비디오 제목", example = "파이트 클럽 공식 예고편")
+    private String name;
+    
+    @Schema(description = "비디오 키", example = "BdJKm16Co6M")
+    private String key;
+    
+    @Schema(description = "비디오 사이트", example = "YouTube")
+    private String site;
+    
+    @Schema(description = "비디오 유형", example = "Trailer")
+    private String type;
 } 
