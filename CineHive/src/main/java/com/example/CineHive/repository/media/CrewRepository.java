9fbebf57a44fac498ef32db843a8e636982b940d
Jong MIn <unib335@naver.com>
2025-04-18 21:49:47 +0900

    [Ref]: CrewRepository에서 특정 직무별 검색 메서드 제거
    - CrewRepository 인터페이스에서 특정 직무별 검색을 위한 메서드(findByMediaIdAndMediaTypeAndJob) 삭제

--- CineHive/src/main/java/com/example/CineHive/repository/media/CrewRepository.java
+++ CineHive/src/main/java/com/example/CineHive/repository/media/CrewRepository.java
@@ -10,7 +10,4 @@ import java.util.List;
 @Repository
 public interface CrewRepository extends JpaRepository<Crew, Long> {
     List<Crew> findByMediaIdAndMediaType(Long mediaId, Media.MediaType mediaType);
-    
-    // 특정 직무별 검색 (ex: 감독만 검색)
-    List<Crew> findByMediaIdAndMediaTypeAndJob(Long mediaId, Media.MediaType mediaType, String job);
 } 
