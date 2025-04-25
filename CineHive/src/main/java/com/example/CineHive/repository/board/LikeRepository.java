634973ce7a90ba2ebe031856ce0be5bd691a5dbb
ssb3204 <ryanp3@naver.com>
2025-04-26 02:22:05 +0900

    [Add]: 사용자를 통해 board 좋아요 찾기

--- CineHive/src/main/java/com/example/CineHive/repository/board/LikeRepository.java
+++ CineHive/src/main/java/com/example/CineHive/repository/board/LikeRepository.java
@@ -5,10 +5,11 @@ import com.example.CineHive.entity.board.Board;
 import com.example.CineHive.entity.board.BoardLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<BoardLike, Long> {
     Optional<BoardLike> findByUserAndBoard(User user, Board board);
-
     long countByBoard(Board board);
+    List<BoardLike> findByUser(User user);
 }
