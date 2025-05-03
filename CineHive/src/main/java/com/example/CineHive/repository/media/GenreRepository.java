521f00e0406df782eb59d19634a70098caedf7e9
Jong MIn <unib335@naver.com>
2025-05-03 15:47:29 +0900

    [Ref]: GenreRepository에서 findByIdAndMediaType 메서드 deprecated 처리 및 MediaGenreRepository에 페이징 기능 추가

--- CineHive/src/main/java/com/example/CineHive/repository/media/GenreRepository.java
+++ CineHive/src/main/java/com/example/CineHive/repository/media/GenreRepository.java
@@ -9,6 +9,13 @@ import java.util.Optional;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Integer> {
+    /**
+     * @deprecated 모든 장르가 COMMON으로 통합되었으므로 findById()를 사용하세요.
+     */
+    @Deprecated
     Optional<Genre> findByIdAndMediaType(Integer id, Genre.MediaType mediaType);
-    List<Genre> findAllByMediaType(Genre.MediaType mediaType);
+    
+    // 필요한 경우 아래 메서드로 대체
+    @Override
+    Optional<Genre> findById(Integer id);
 } 
