80f4e13141b94f205e3dc55e5c236a9c9e1969b7
Jong MIn <unib335@naver.com>
2025-06-11 03:48:55 +0900

    [Add]: TvSeriesRepository 인터페이스 추가
    - TV 시리즈 정보를 처리하기 위한 TvSeriesRepository 인터페이스 추가
    - JpaRepository를 상속받아 기본 CRUD 기능 제공

---
+++ CineHive/src/main/java/com/example/CineHive/repository/media/TvSeriesRepository.java
@@ -0,0 +1,7 @@
+package com.example.CineHive.repository.media;
+
+import com.example.CineHive.entity.media.TvSeries;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TvSeriesRepository extends JpaRepository<TvSeries, Long> {
+}
