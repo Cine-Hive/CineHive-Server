a80edfb024aac4eb25ebeb95a03702074e73cb4c
Jong MIn <unib335@naver.com>
2025-07-11 19:38:09 +0900

    [Add]: ErrorResponse 클래스에 path 필드 추가
    - ErrorResponse 클래스의 생성자에 path 필드를 추가하여 오류 응답에 대한 경로 정보를 포함하도록 수정

--- CineHive/src/main/java/com/example/CineHive/dto/response/ErrorResponse.java
+++ CineHive/src/main/java/com/example/CineHive/dto/response/ErrorResponse.java
@@ -9,10 +9,12 @@ public class ErrorResponse {
     private final int status;
     private final String error;
     private final String message;
+    private final String path;
 
-    public ErrorResponse(int status, String error, String message) {
+    public ErrorResponse(int status, String error, String message, String path) {
         this.status = status;
         this.error = error;
         this.message = message;
+        this.path = path;
     }
 }
