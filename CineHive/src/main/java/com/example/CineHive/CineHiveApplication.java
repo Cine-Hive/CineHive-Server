6923815dcc25dc27db47c6554975689bd2aa998b
Jong MIn <unib335@naver.com>
2025-07-15 00:55:02 +0900

    [Add]: JPA 감사 기능 활성화
    - CineHiveApplication 클래스에 @EnableJpaAuditing 어노테이션을 추가하여 JPA 감사 기능을 활성화
    - 데이터 변경 이력 추적 및 관리 기능을 통해 애플리케이션의 데이터 무결성 향상

--- CineHive/src/main/java/com/example/CineHive/CineHiveApplication.java
+++ CineHive/src/main/java/com/example/CineHive/CineHiveApplication.java
@@ -2,10 +2,12 @@ package com.example.CineHive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @EnableAsync
 @EnableScheduling
 public class CineHiveApplication {
