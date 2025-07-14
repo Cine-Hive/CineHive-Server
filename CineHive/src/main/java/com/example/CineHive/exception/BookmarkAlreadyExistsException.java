0213a0477d4b5db90bb6610520480c23baf1f56e
Jong MIn <unib335@naver.com>
2025-07-15 04:15:51 +0900

    [Ref]: BookmarkAlreadyExistsException 클래스 수정
    - BusinessException을 RuntimeException으로 변경하여 예외 처리 방식을 개선
    - 예외 메시지를 한국어로 수정하여 사용자 친화성 향상

--- CineHive/src/main/java/com/example/CineHive/exception/BookmarkAlreadyExistsException.java
+++ CineHive/src/main/java/com/example/CineHive/exception/BookmarkAlreadyExistsException.java
@@ -1,9 +1,10 @@
 package com.example.CineHive.exception;
 
-public class BookmarkAlreadyExistsException extends BusinessException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+public class BookmarkAlreadyExistsException extends RuntimeException {
     public BookmarkAlreadyExistsException(Long memberId, Long boardId) {
-        super(String.format("Member %d has already bookmarked Board %d", memberId, boardId),
-                ErrorCode.BOOKMARK_ALREADY_EXISTS);
+        super("회원(ID: " + memberId + ")은(는) 이미 게시글(ID: " + boardId + ")을 북마크했습니다.");
     }
 }
