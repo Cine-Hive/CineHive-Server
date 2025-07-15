5c2ed50f75d05fcdbb4e0fc7f57df4844e990ba4
Jong MIn <unib335@naver.com>
2025-07-15 17:08:59 +0900

    [Add]: OAuthCommunicationException 클래스 추가
    - OAuth 통신 중 발생할 수 있는 예외를 처리하기 위한 OAuthCommunicationException 클래스를 추가하여 예외 처리 로직을 강화함

---
+++ CineHive/src/main/java/com/example/CineHive/exception/OAuthCommunicationException.java
@@ -0,0 +1,7 @@
+package com.example.CineHive.exception;
+
+public class OAuthCommunicationException extends RuntimeException {
+    public OAuthCommunicationException(String message) {
+        super(message);
+    }
+}
