1fd030078e32be19eaafbf28d4895e0a516d4130
Jong MIn <unib335@naver.com>
2025-07-16 11:03:01 +0900

    [Ref]: MediaSyncService 인터페이스에 주석 추가
    - MediaSyncService 클래스에 미디어 동기화 관련 설명 주석을 추가하여 코드 이해도를 높임

--- CineHive/src/main/java/com/example/CineHive/service/media/MediaSyncService.java
+++ CineHive/src/main/java/com/example/CineHive/service/media/MediaSyncService.java
@@ -1,5 +1,9 @@
 package com.example.CineHive.service.media;
 
+/**
+ * MediaSyncService 미디어 동기화 관련 서비스입니다.
+ * 실제 데이터베이스와 상호작용하며 미디어 동기화관련 비즈니스 로직을 수행합니다.
+ */
 public interface MediaSyncService {
     void syncPopularMovies(int page);
     void syncTopRatedTvSeries(int page);
