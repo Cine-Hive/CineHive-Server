ba7adc4b42cd212ba39a50dfa30d4de57d4eb73c
Jong MIn <unib335@naver.com>
2025-07-20 16:44:53 +0900

    [Ref]: LoginHistoryDto 수정 및 개선
    - LoginHistoryDto 클래스를 record로 변경하여 코드 간결성을 높임.
    - Swagger 어노테이션을 추가하여 API 문서화 개선.
    - 필드 이름을 userId, userNickname, createdAt, updatedAt으로 변경하여 일관성 유지.
    - LoginHistory 엔티티에서 DTO로 변환하는 from 메서드를 추가하여 데이터 변환 로직을 명확히 함.

--- CineHive/src/main/java/com/example/CineHive/dto/user/LoginHistoryDto.java
+++ CineHive/src/main/java/com/example/CineHive/dto/user/LoginHistoryDto.java
@@ -1,23 +1,27 @@
 package com.example.CineHive.dto.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.example.CineHive.entity.user.LoginHistory;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class LoginHistoryDto {
-    private Long id;
-
-    private Long memberId;
-    private String memberNickname;
-
-    private LocalDateTime firstLoginDate;
-    private LocalDateTime lastLoginDate;
-    private String browser;
+@Schema(description = "로그인 이력 응답 DTO")
+public record LoginHistoryDto(
+        Long id,
+        Long userId,
+        String userNickname,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt,
+        String browser
+) {
+    public static LoginHistoryDto from(LoginHistory loginHistory) {
+        return new LoginHistoryDto(
+                loginHistory.getId(),
+                loginHistory.getUser().getId(),
+                loginHistory.getUser().getNickname(),
+                loginHistory.getCreatedAt(),
+                loginHistory.getUpdatedAt(),
+                loginHistory.getBrowser()
+        );
+    }
 }
