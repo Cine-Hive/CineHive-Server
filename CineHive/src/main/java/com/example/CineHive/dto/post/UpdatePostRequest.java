f8bf6822b6317fc10443fa8848afeb7245b32168
Jong MIn <unib335@naver.com>
2025-07-20 16:48:50 +0900

    [Add]: 게시글 수정 요청 DTO 클래스 추가
    - UpdatePostRequest 클래스를 추가하여 게시글 수정에 필요한 제목과 내용을 포함함.
    - 제목과 내용에 대한 유효성 검사를 위한 어노테이션을 적용하여 입력값의 유효성을 보장함.

--- CineHive/src/main/java/com/example/CineHive/dto/post/UpdatePostRequest.java
+++ CineHive/src/main/java/com/example/CineHive/dto/post/UpdatePostRequest.java
@@ -1,20 +1,17 @@
-package com.example.CineHive.dto.board;
+package com.example.CineHive.dto.post;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-/**
- * 게시글 수정을 위한 요청 데이터를 담는 DTO입니다.
- */
 @Schema(description = "게시글 수정 요청 DTO")
-public record UpdateBoardRequest(
+public record UpdatePostRequest(
         @Schema(description = "수정할 게시글 제목", example = "'듄 파트2' N차 관람 후기")
         @NotBlank(message = "제목은 필수 입력 항목입니다.")
         @Size(max = 100, message = "제목은 100자를 초과할 수 없습니다.")
-        String brdTitle,
+        String title,
 
         @Schema(description = "수정할 게시글 내용", example = "볼 때마다 새로운 점이 보이네요.")
         @NotBlank(message = "내용은 필수 입력 항목입니다.")
-        String brdContent
+        String content
 ) {}
