006e51792a5d327c21181bd39f4b8579367893f9
Jong MIn <unib335@naver.com>
2025-07-20 15:47:53 +0900

    [Add]: 회원 유형을 정의하는 UserType Enum 클래스 추가

---
+++ CineHive/src/main/java/com/example/CineHive/entity/user/UserType.java
@@ -0,0 +1,19 @@
+package com.example.CineHive.entity.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 회원의 유형(분류)을 정의하는 Enum 클래스.
+ * 권한(Role)과 별개로, 비즈니스 로직을 분기 처리하기 위해 사용됩니다.
+ */
+@Getter
+@RequiredArgsConstructor
+public enum UserType {
+
+    GENERAL("일반 회원"),
+    CRITIC("평론가"),
+    PAID("유료 회원");
+
+    private final String description;
+}
