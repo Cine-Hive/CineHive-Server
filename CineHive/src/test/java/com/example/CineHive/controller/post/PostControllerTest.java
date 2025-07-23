e8c67c59c5bbaee8ce1eec3ccf82bad165deb670
Jong MIn <unib335@naver.com>
2025-07-23 12:22:25 +0900

    [Add]: BookmarkControllerTest 및 PostControllerTest 추가
    - 북마크 기능에 대한 통합 테스트를 BookmarkControllerTest로 추가하여 북마크 추가, 취소 및 상태 조회 기능을 검증함.
    - 게시글 관련 API의 통합 테스트를 PostControllerTest로 추가하여 게시글 생성, 조회, 수정 및 삭제 기능을 검증함.
    - 각 테스트 케이스에 대한 성공 및 실패 시나리오를 포함하여 API의 동작을 철저히 검증함.

--- CineHive/src/test/java/com/example/CineHive/controller/post/PostControllerTest.java
+++ CineHive/src/test/java/com/example/CineHive/controller/post/PostControllerTest.java
@@ -1,4 +1,4 @@
-package com.example.CineHive.controller.board;
+package com.example.CineHive.controller.post;
 
 import com.example.CineHive.entity.post.Post;
 import com.example.CineHive.entity.user.*;
