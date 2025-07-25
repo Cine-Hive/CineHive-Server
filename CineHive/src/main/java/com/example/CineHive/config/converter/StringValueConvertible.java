745dce1f94763217560e7e14e702822473627f3d
Jong MIn <unib335@naver.com>
2025-07-26 03:02:05 +0900

    [Ref]: WebConfig 및 Converter 관련 코드 정리
    - StringToEnumConverterFactory 및 StringValueConvertible 인터페이스 삭제.
    - WebConfig에서 관련 설정 및 주석 정리하여 코드 가독성을 향상시킴.

--- CineHive/src/main/java/com/example/CineHive/config/converter/StringValueConvertible.java
+++
@@ -1,8 +0,0 @@
-package com.example.CineHive.config.converter;
-
-/**
- * 클라이언트로부터 받은 문자열 값을 Enum 상수로 변환할 수 있는 모든 Enum이 구현해야 하는 인터페이스.
- */
-public interface StringValueConvertible {
-    String getClientValue();
-}
