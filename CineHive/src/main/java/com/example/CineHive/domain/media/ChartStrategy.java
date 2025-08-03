54558cc009558f52be1f0313171ecfb9903778d7
Jong MIn <unib335@naver.com>
2025-08-03 15:58:43 +0900

    [Ref]: ChartStrategy 인터페이스 수정
    - PagedResponse를 PageResponse로 변경하여 DTO 클래스 이름을 일관되게 수정함으로써 코드의 가독성을 향상시킴
    - 주석을 통해 메서드의 기능을 명확히 설명하여 이해도를 높임

--- CineHive/src/main/java/com/example/CineHive/domain/media/ChartStrategy.java
+++ CineHive/src/main/java/com/example/CineHive/domain/media/ChartStrategy.java
@@ -1,9 +1,8 @@
 package com.example.CineHive.domain.media;
 
 import com.example.CineHive.client.tmdb.TmdbApiClient;
-import com.example.CineHive.domain.common.dto.PagedResponse;
+import com.example.CineHive.domain.common.dto.PageResponse;
 import com.example.CineHive.domain.media.dto.MediaChartResponse;
-import reactor.core.publisher.Mono;
 
 @FunctionalInterface
 public interface ChartStrategy {
@@ -13,5 +12,5 @@ public interface ChartStrategy {
      * @param page      요청할 페이지 번호
      * @return 페이징된 MediaChartResponse
      */
-    PagedResponse<MediaChartResponse> fetchChart(TmdbApiClient client, int page);
+    PageResponse<MediaChartResponse> fetchChart(TmdbApiClient client, int page);
 }
