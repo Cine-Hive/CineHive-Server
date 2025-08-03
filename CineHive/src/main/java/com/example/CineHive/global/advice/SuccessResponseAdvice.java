ded2a3745eca5931a362cc582eb8e528456dfe1d
Jong MIn <unib335@naver.com>
2025-08-03 15:47:46 +0900

    [Add]: SuccessResponseAdvice 클래스 추가
    - API 응답을 일관되게 처리하기 위한 SuccessResponseAdvice 클래스를 추가하여, 성공적인 응답을 ApiResponse 형식으로 변환하는 기능을 구현함

---
+++ CineHive/src/main/java/com/example/CineHive/global/advice/SuccessResponseAdvice.java
@@ -0,0 +1,33 @@
+package com.example.CineHive.domain.common.dto;
+
+import com.example.CineHive.domain.common.dto.ApiResponse;
+import com.example.CineHive.global.exception.GlobalExceptionHandler;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice(basePackages = "com.example.CineHive")
+public class SuccessResponseAdvice implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return !returnType.getContainingClass().equals(GlobalExceptionHandler.class) &&
+                !returnType.getParameterType().equals(ApiResponse.class) &&
+                !ResponseEntity.class.isAssignableFrom(returnType.getParameterType());
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request, ServerHttpResponse response) {
+        if (returnType.getParameterType().equals(void.class)) {
+            return null;
+        }
+        return ApiResponse.ok(body);
+    }
+}
