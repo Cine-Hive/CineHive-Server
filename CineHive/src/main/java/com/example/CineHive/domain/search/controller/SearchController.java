daaf3dc0a709092bcaed695cb3db8d5feeab3eed
Jong MIn <unib335@naver.com>
2025-08-05 22:04:04 +0900

    [Add]: 통합 검색 API 컨트롤러 추가
    - 다양한 도메인(영화, TV, 게시글, 인물 등)에 대한 검색 기능을 제공하는 SearchController 클래스 구현
    - 각 검색 기능에 대한 API 엔드포인트 및 TODO 주석 추가

--- CineHive/src/main/java/com/example/CineHive/domain/search/controller/SearchController.java
+++ CineHive/src/main/java/com/example/CineHive/domain/search/controller/SearchController.java
@@ -1,4 +1,4 @@
-package com.example.CineHive.domain.search;
+package com.example.CineHive.domain.search.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
