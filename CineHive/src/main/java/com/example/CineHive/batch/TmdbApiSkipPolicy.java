e73849194f2f2cca5fa29c1cc7831670b80bb2fd
Jong MIn <unib335@naver.com>
2025-08-07 01:29:20 +0900

    [Add]: TmdbApiSkipPolicy 클래스 추가
    - TMDB API 예외에 대한 커스텀 Skip 정책 구현
    - TmdbClientException의 HTTP 상태가 NOT_FOUND일 경우 스킵 처리

---
+++ CineHive/src/main/java/com/example/CineHive/batch/TmdbApiSkipPolicy.java
@@ -0,0 +1,19 @@
+package com.example.CineHive.batch;
+
+import com.example.CineHive.global.exception.TmdbClientException;
+import org.springframework.batch.core.step.skip.SkipLimitExceededException;
+import org.springframework.batch.core.step.skip.SkipPolicy;
+import org.springframework.http.HttpStatus;
+
+/**
+ * TMDB API 예외에 대한 커스텀 Skip 정책입니다.
+ */
+public class TmdbApiSkipPolicy implements SkipPolicy {
+    @Override
+    public boolean shouldSkip(Throwable t, long stepExecutionId) throws SkipLimitExceededException { // int skipCount -> long stepExecutionId
+        if (t instanceof TmdbClientException ex) {
+            return ex.getHttpStatus() == HttpStatus.NOT_FOUND;
+        }
+        return false;
+    }
+}
