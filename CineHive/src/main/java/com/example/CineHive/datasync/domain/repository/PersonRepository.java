87b2136c3541912522bc64839986e83bb3aa96e5
Jong MIn <unib335@naver.com>
2025-08-13 15:45:17 +0900

    [Add]: PersonRepository에 @Repository 어노테이션 추가
    - PersonRepository 인터페이스에 @Repository("syncPersonRepository") 어노테이션을 추가하여 Spring의 컴포넌트 스캔에 포함되도록 설정함

--- CineHive/src/main/java/com/example/CineHive/datasync/domain/repository/PersonRepository.java
+++ CineHive/src/main/java/com/example/CineHive/datasync/domain/repository/PersonRepository.java
@@ -2,6 +2,8 @@ package com.example.CineHive.datasync.domain.repository;
 
 import com.example.CineHive.datasync.domain.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("syncPersonRepository")
 public interface PersonRepository extends JpaRepository<Person, Long> {
 }
