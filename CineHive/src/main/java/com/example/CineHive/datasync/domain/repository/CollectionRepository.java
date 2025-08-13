8c44e6016c54de538ebb68d730f62cfbf312d26d
Jong MIn <unib335@naver.com>
2025-08-14 05:23:16 +0900

    [Ref]: CollectionRepository 인터페이스 수정
    - CollectionRepository 인터페이스에 @Repository 어노테이션을 추가하여 Spring의 컴포넌트 스캔에 포함되도록 변경함.
    - 코드의 가독성을 높이고, Spring Data JPA의 기능을 활용할 수 있도록 개선함.

--- CineHive/src/main/java/com/example/CineHive/datasync/domain/repository/CollectionRepository.java
+++ CineHive/src/main/java/com/example/CineHive/datasync/domain/repository/CollectionRepository.java
@@ -2,6 +2,8 @@ package com.example.CineHive.datasync.domain.repository;
 
 import com.example.CineHive.datasync.domain.entity.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("syncCollectionRepository")
 public interface CollectionRepository extends JpaRepository<Collection, Long> {
 }
