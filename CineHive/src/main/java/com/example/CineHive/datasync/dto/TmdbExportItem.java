7513ac8c5666a33f45b7616191ce8f9a31c383a6
Jong MIn <unib335@naver.com>
2025-08-13 19:34:16 +0900

    [Add]: TmdbExportItem DTO에 우선순위 및 건너뛰기 메서드 추가
    - TmdbExportItem DTO에 영화의 우선순위를 결정하는 getPriority 메서드와 성인 콘텐츠 여부에 따라 건너뛰기를 판단하는 shouldSkip 메서드를 추가함.
    - 인기(popularity) 값에 따라 우선순위를 설정하고, 성인 콘텐츠 여부를 확인하는 로직을 구현함.

--- CineHive/src/main/java/com/example/CineHive/datasync/dto/TmdbExportItem.java
+++ CineHive/src/main/java/com/example/CineHive/datasync/dto/TmdbExportItem.java
@@ -7,5 +7,20 @@ public record TmdbExportItem(
         @JsonProperty("original_title") String originalTitle,
         @JsonProperty("original_name") String originalName,
         boolean adult,
-        double popularity
-) {}
+        double popularity,
+        @JsonProperty("video") Boolean video
+) {
+    public Integer getPriority() {
+        if (popularity > 100.0) {
+            return 10;
+        }
+        if (popularity > 50.0) {
+            return 5;
+        }
+        return 0;
+    }
+
+    public boolean shouldSkip() {
+        return adult;
+    }
+}
