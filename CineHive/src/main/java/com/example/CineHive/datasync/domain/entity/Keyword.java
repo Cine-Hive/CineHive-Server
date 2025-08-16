8a96eb41b69f118f69d63f0bbce8ef9535dbaa12
Jong MIn <unib335@naver.com>
2025-08-17 03:15:09 +0900

    [Ref]: Keyword 엔티티의 테이블 이름 변경 및 필드 길이 조정
    - Keyword 클래스의 테이블 이름을 "keywords"에서 "keyword"로 변경함.
    - name 필드의 길이를 128로 설정하여 데이터베이스 제약 조건을 강화함.

--- CineHive/src/main/java/com/example/CineHive/datasync/domain/entity/Keyword.java
+++ CineHive/src/main/java/com/example/CineHive/datasync/domain/entity/Keyword.java
@@ -1,22 +1,21 @@
 package com.example.CineHive.datasync.domain.entity;
 
-import com.example.CineHive.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "keywords")
+@Table(name = "keyword")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @ToString
-public class Keyword extends BaseEntity {
+public class Keyword {
     
     @Id
     @Column(name = "tmdb_id")
     private Long tmdbId;
     
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, length = 128)
     private String name;
 }
